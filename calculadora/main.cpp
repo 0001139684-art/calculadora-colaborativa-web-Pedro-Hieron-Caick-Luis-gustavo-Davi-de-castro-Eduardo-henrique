161165a72dda40b9d94f2f6f4a722d77908b7ae9
0001139684-art <0001139684@senaimgaluno.com.br>
2025-08-06 13:38:20 -0300

    Create main.cpp
    feat: Adiciona estrutura inicial do projeto C++

---
+++ calculadora/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Calculadora Colaborativa em C++" << endl;
+    // As funcionalidades serão adicionadas aqui.
+    return 0;
+}
+
+// Funções a serem implementadas por cada integrante:
+// - Soma
+// - Subtração
+// - Multiplicação
+// - Divisão
