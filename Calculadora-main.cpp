a90c7886745662ecff6d05fd726f8cf9e0312848
0001139684-art <0001139684@senaimgaluno.com.br>
2025-08-06 14:22:14 -0300

    Update and rename README.md to Calculadora-main.cpp
    feat: Adiciona estrutura inicial do projeto C++

---
+++ Calculadora-main.cpp
@@ -0,0 +1,17 @@
+# calculadora-colaborativa-web-Pedro-Hieron-Caick-Luis-gustavo-Davi-de-castro-Eduardo-henrique
+"Projeto de calculadora colaborativa em C# (apenas web) para a disciplina de controle de versão e colaboração"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Calculadora Colaborativa em C++" << endl;
+    // As funcionalidades serão adicionadas aqui
+    return 0;
+}
+
+// Funções a serem implementadas por cada integrante:
+// double Soma(double a, double b);
+// double Subtracao(double a, double b);
+// double Multiplicacao(double a, double b);
+// double Divisao(double a, double b);
--- README.md
+++
@@ -1,2 +0,0 @@
-# calculadora-colaborativa-web-Pedro-Hieron-Caick-Luis-gustavo-Davi-de-castro-Eduardo-henrique
-"Projeto de calculadora colaborativa em C# (apenas web) para a disciplina de controle de versão e colaboração"
